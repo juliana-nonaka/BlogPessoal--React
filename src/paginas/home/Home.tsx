5f914709916c62de7924128e53ccb53e32fc2b2f
juliana.nonaka <juliananonakaudvari@gmail.com>
2022-10-27 20:18:37 -0300

    Inicializando projeto com React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://cinemacomrapadura.com.br/imagens/2013/01/continue-a-nadar-procurando-nemo.jpg" alt="Imagem Tela Inicial" className="img"/>
+        </>
+    );
+}
+
+export default Home;
