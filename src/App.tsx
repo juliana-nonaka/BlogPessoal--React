cd893df5a3ff9d58980281234916643f6c7d1624
juliana.nonaka <juliananonakaudvari@gmail.com>
2022-11-04 17:10:44 -0300

    Inclusão das páginas Model e Service

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,32 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
+import {Grid} from '@material-ui/core';
 import Home from './paginas/home/Home';
 import Login from './paginas/login/Login';
+import logo from './logo.svg';
 import './App.css';
 
-
-
 function App() {
-  return(
+  return (      
     <Router>
     <Navbar />
-    <div style={{ minHeight: '100vh' }}>
-    <Routes> // Antigo Switch
-    
-    <Route path="/" element={<Login />} />
-    <Route path="/login" element={<Login />} />
-    <Route path="/home" element={<Home />} />
-    
+    <Routes>
+
+    <Route path="/" element={<Login  />} />
+
+<Route path="/home" element={<Home />} />
+
+<Route path="/login" element={<Login />} />
+
+      <Route path="/cadastrousuario" element={<CadastroUsuario />} />
     </Routes>
-    </div>
     <Footer />
-    </Router>
-    );
+  </Router>   
+       
+  );
 }
 
 export default App;
--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,46 +1,48 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
-import {Link} from 'react-router-dom';
+import {AppBar, Toolbar, Typography} from '@material-ui/core'
+import { Link } from 'react-router-dom';
 import './Navbar.css';
-function Navbar() {
+import{Box} from '@mui/material';
+
+function Navbar (){
     return (
         <>
-            <AppBar position="static">
+               <AppBar position="static">
                 <Toolbar variant="dense">
-                    <Box className='cursor'>
+                    <Box style={{ cursor: "pointer" }} >
                         <Typography variant="h5" color="inherit">
                             BlogPessoal
                         </Typography>
                     </Box>
 
                     <Box display="flex" justifyContent="start">
-                        <Box mx={1} className='cursor'>
+                        <Box mx={1} style={{ cursor: "pointer" }}>
                             <Typography variant="h6" color="inherit">
                                 home
                             </Typography>
                         </Box>
-                        <Box mx={1} className='cursor'>
+                        <Box mx={1} style={{ cursor: "pointer" }}>
                             <Typography variant="h6" color="inherit">
                                 postagens
                             </Typography>
                         </Box>
-                        <Box mx={1} className='cursor'>
+                        <Box mx={1} style={{ cursor: "pointer" }}>
                             <Typography variant="h6" color="inherit">
                                 temas
                             </Typography>
                         </Box>
-                        <Box mx={1} className='cursor'>
+                        <Box mx={1} style={{ cursor: "pointer" }}>
                             <Typography variant="h6" color="inherit">
                                 cadastrar tema
                             </Typography>
                         </Box>
-                            <Link to='/login' className='text-decorator-none'>
-                            <Box mx={1} className='cursor'>
-                            <Typography variant="h6" color="inherit">
+                        <Link to='/login' className='text-decorator-none'>
+                            <Box mx={1} style={{ cursor: "pointer", color: "white" }}>
+                                <Typography variant="h6" color="inherit">
                                     logout
                                 </Typography>
                             </Box>
-                            </Link>
+                        </Link>
                         
                     </Box>
 
